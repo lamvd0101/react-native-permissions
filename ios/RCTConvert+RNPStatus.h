7a5940e9dc6680b6f5d497d069bbe7a738248ca3
Vương Đức Lâm <lamvd0101@gmail.com>
2019-12-17 14:45:08 +0700

    Remove permissions

--- ios/RCTConvert+RNPStatus.h
+++ ios/RCTConvert+RNPStatus.h
@@ -22,19 +22,10 @@ static NSString* RNPStatusRestricted = @"restricted";
 
 typedef NS_ENUM(NSInteger, RNPType) {
     RNPTypeUnknown,
-    RNPTypeLocation,
     RNPTypeCamera,
     RNPTypeMicrophone,
     RNPTypePhoto,
-    RNPTypeContacts,
-    RNPTypeEvent,
-    RNPTypeReminder,
-    RNPTypeBluetooth,
-    RNPTypeNotification,
-    RNPTypeBackgroundRefresh,
-    RNPTypeSpeechRecognition,
-    RNPTypeMediaLibrary,
-    RNPTypeMotion
+    RNPTypeMediaLibrary
 };
 
 @interface RCTConvert (RNPStatus)
